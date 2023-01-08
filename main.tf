a7ed6d5ec33632aa1fff861e23a9015e7541d5f6
nmcostello <nmcos16@gmail.com>
2023-01-08 13:45:52 -0500

    trying new state key

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "challenge-tf-state"
-    key    = "terraform.tfstate"
+    key    = "/terraform.tfstate"
     region = "us-east-1"
   }
 }
