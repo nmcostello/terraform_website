15a1186378b17446aaf81dcfa7a9cc8d324cea25
nmcostello <nmcos16@gmail.com>
2023-01-16 22:42:16 -0500

    fixed string interpolation

--- test/validate.py
+++ test/validate.py
@@ -3,15 +3,15 @@ import os
 
 def main(url: str):
     # Request https
-    httpsUrl = f("https://{url}")
+    httpsUrl = f"https://{url}"
     httpsRequest = requests.get(url)
     # Request http
-    httpUrl = f("http://{url}")
+    httpUrl = f"http://{url}"
     httpRequest = requests.get(url)
 
     print("HTTP= ", httpRequest.status_code)
     print("HTTPS= ",httpRequest.status_code)
-    
+
 if __name__ == "__main__":
 
     url = os.environ.get('URL')
