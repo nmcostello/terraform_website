9024cc319eb9ae9ee806df25442813319bb72bf0
nmcostello <nmcos16@gmail.com>
2023-01-16 22:17:22 -0500

    first run of python script

---
+++ test/validate.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+def main(url: str):
+    # Request https
+    httpsUrl = f("https://{url}")
+    httpsRequest = requests.get(url)
+    # Request http
+    httpUrl = f("http://{url}")
+    httpRequest = requests.get(url)
+
+    print("HTTP= ", httpRequest.status_code)
+    print("HTTPS= ",httpRequest.status_code)
+if __name__ == "__main__":
+
+    url = os.environ.get('URL')
+    main(url)
