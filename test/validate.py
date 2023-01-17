275d8338bae7f7f4b215109f576b9c4c7e8c877c
nmcostello <nmcos16@gmail.com>
2023-01-16 22:47:28 -0500

    fix requests url

--- test/validate.py
+++ test/validate.py
@@ -4,10 +4,10 @@ import os
 def main(url: str):
     # Request https
     httpsUrl = f"https://{url}"
-    httpsRequest = requests.get(url)
+    httpsRequest = requests.get(httpsUrl)
     # Request http
     httpUrl = f"http://{url}"
-    httpRequest = requests.get(url)
+    httpRequest = requests.get(httpUrl)
 
     print("HTTP= ", httpRequest.status_code)
     print("HTTPS= ",httpRequest.status_code)
